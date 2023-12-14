4a2cef6ee0ff61161ca912dc8bfa1b23e06dd696
Brett Etter <brett.etter@staratlas.com>
2023-12-14 14:30:57 -0600

    Day 4 Part 2

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -33,14 +33,21 @@ impl Card {
         }
     }
 
-    pub fn value(&self) -> u32 {
+    pub fn winning_count(&self) -> u32 {
         let mut winning_count: u32 = 0;
         for my_number in &self.my_numbers {
             if self.winning_numbers.contains(my_number) {
                 winning_count += 1;
             }
         }
-        winning_count.checked_sub(1).map(|x| 1 << x).unwrap_or(0)
+        winning_count
+    }
+
+    pub fn value(&self) -> u32 {
+        self.winning_count()
+            .checked_sub(1)
+            .map(|x| 1 << x)
+            .unwrap_or(0)
     }
 }
 
@@ -48,4 +55,16 @@ pub fn day4() {
     let cards = INPUT.lines().map(Card::parse).collect::<Vec<_>>();
     let sum = cards.iter().map(Card::value).sum::<u32>();
     println!("Day 4 part 1: {}", sum);
+
+    let mut card_multipliers = (0..cards.len()).map(|_| 1u32).collect::<Vec<_>>();
+    for (index, card) in cards.iter().enumerate() {
+        let multiplier = card_multipliers[index];
+        for other_index in (index + 1).min(card_multipliers.len())
+            ..=(index + card.winning_count() as usize).min(card_multipliers.len() - 1)
+        {
+            card_multipliers[other_index] += multiplier;
+        }
+    }
+    let sum = card_multipliers.iter().sum::<u32>();
+    println!("Day 4 part 2: {}", sum);
 }
